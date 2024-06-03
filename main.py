572b0237562b48b7f4979936e66768ba1e5981b7
ilyas777rnd <Sulinand@homtail.com>
2024-06-04 00:06:23 +0300

    births dataset

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import matplotlib.pyplot as plt
 
 # импортируем файл с данными о пассажирах
 passengers = pd.read_csv("passengers.csv")
+births = pd.read_csv("births.csv")
 passengers.set_index('Month', inplace = True)
 passengers.index = pd.to_datetime(passengers.index)
+births.set_index('Date', inplace=True)
+births.index = pd.to_datetime(births.index)
 
 # зададим размер графика
 plt.figure(figsize=(15, 8))
 
 # поочередно зададим кривые (перевозки и скользящее среднее) с подписями и цветом
-plt.plot(passengers, label='Перевозки пассажиров по месяцам', color='steelblue')
-plt.plot(passengers.rolling(window=12).mean(), label='Скользящее среднее за 12 месяцев', color='orange')
+# plt.plot(passengers, label='Перевозки пассажиров по месяцам', color='steelblue')
+# plt.plot(passengers.rolling(window=12).mean(), label='Скользящее среднее за 12 месяцев', color='orange')
+
+plt.plot(births, label='Показатели рождаемости по месяцам', color='steelblue')
+plt.plot(births.rolling(window=12).mean(), label='Скользящее среднее за 12 месяцев', color='orange')
 
 # добавим легенду, ее положение на графике и размер шрифта
 plt.legend(title='', loc='upper left', fontsize=14)
@@ -21,7 +27,7 @@ plt.legend(title='', loc='upper left', fontsize=14)
 # добавим подписи к осям и заголовки
 plt.xlabel('Месяцы', fontsize=14)
 plt.ylabel('Количество пассажиров', fontsize=14)
-plt.title('Перевозки пассажиров с 1949 по 1960 год', fontsize=16)
+plt.title('Рождаемость с 1949 по 1960 год', fontsize=16)
 
 # выведем обе кривые на одном графике
 plt.show()
